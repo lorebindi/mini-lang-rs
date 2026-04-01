0a166480a3f2d1b1ba86b5cb78c79a156a452040
lorenzo <lorenzo.bindi02@gmail.com>
2026-04-01 16:58:06 +0200

    Added some tests to miniimp and moved them in a dedicated directory.

--- miniimp/src/main.rs
+++ miniimp/src/main.rs
@@ -1,13 +1,15 @@
 mod parser;
 pub mod ast;
-mod test;
 
 fn main() {
     let program = r#"
         def main with input x output a as
-            a := x + 1 + 2
+            a := ap
             "#;
     if let Some(prog) = parser::parse_program(program) {
         println!("{:#?}", prog);
+    }
+    else{
+        println!("The program can't be parsed.");
     }
 }
